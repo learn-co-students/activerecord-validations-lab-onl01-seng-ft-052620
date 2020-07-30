fd252ab3ca4ae499e5fab64cc2fb5ef56222d04d
shazy89 <erdo.shazy123@gmail.com>
2020-07-30 14:34:57 -0400

    Done.

---
+++ app/validators/clickbait_validator.rb
@@ -0,0 +1,15 @@
+class ClickbaitValidator < ActiveModel::Validator
+
+    def validate(record)
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+           if record.title
+        unless clickbait.any?{|array| record.title.match?(array)} 
+          record.errors[:title] << 'Title is not clickbaity'
+          end
+        end
+      end
+
+end
+
+#Finally, add a custom validator to Post that ensures the title is sufficiently clickbait-y. 
+#If the title does not contain "Won't Believe", "Secret", "Top [number]", or "Guess", the validator should add a validation.
